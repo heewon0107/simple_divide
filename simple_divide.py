c4a3aca478f246a40c51b91d2ea5e5d13da2f05b
shinheewon0107 <qjqtktnf@naver.com>
2024-07-23 22:53:08 +0900

    simple_divide

---
+++ simple_divide.py
@@ -0,0 +1,17 @@
+test_case = int(input())
+n = int(input())
+for i in range(1, test_case + 1):
+    
+    
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+    for x in [2, 3, 5, 7, 11]:
+        while n % x == 0 :
+            a += 1
+            n = n / 2
+        while n % x
+
+        print(e)        
