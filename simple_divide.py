1c90fe1deec385d3480f2247b46fc822bbddeb09
shinheewon0107 <qjqtktnf@naver.com>
2024-07-25 19:24:52 +0900

    간단한 소인수 분해

--- simple_divide.py
+++ simple_divide.py
@@ -1,17 +1,32 @@
 test_case = int(input())
-n = int(input())
+
 for i in range(1, test_case + 1):
-    
-    
+    n = int(input())
+
     a = 0
     b = 0
     c = 0
     d = 0
     e = 0
-    for x in [2, 3, 5, 7, 11]:
-        while n % x == 0 :
-            a += 1
-            n = n / 2
-        while n % x
+           
+    while n % 2 == 0 :
+        n = n / 2
+        a += 1
+          
+    while n % 3 == 0 :
+        n = n / 3 
+        b += 1
+            
+    while n % 5 == 0 :
+        n = n / 5 
+        c += 1
+           
+    while n % 7 == 0 :
+        n = n / 7 
+        d += 1
+            
+    while n % 11 == 0 :
+        n = n / 11
+        e += 1
 
-        print(e)        
+    print(f"#{i} {a} {b} {c} {d} {e}")      
